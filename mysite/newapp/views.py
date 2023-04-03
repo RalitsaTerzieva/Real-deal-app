071a655e5034ba011e046a6a935bf0513da27f49
Ralitsa Terzieva <ralicaterzieva90@gmail.com>
2023-04-03 13:32:52 +0300

    Add view, template and url

--- mysite/newapp/views.py
+++ mysite/newapp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Moviedata
 
-# Create your views here.
+
+
+def movie_list(request):
+    movie_objects = Moviedata.objects.all()
+    return render(request, 'newapp/movie_list.html', {'movie_objects': movie_objects})
+
