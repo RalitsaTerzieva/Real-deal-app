e8190b74e9e34dc8bbb6452532bd9e9ca24c7ca4
Ralitsa Terzieva <ralicaterzieva90@gmail.com>
2023-04-05 16:18:59 +0300

    Use Paginator to create Pagination

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -14,11 +14,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path
 from newapp.views import movie_list
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('movies/', movie_list, name='movies_list'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) 
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- mysite/newapp/views.py
+++ mysite/newapp/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from .models import Moviedata
-
+from django.core.paginator import Paginator
 
 
 def movie_list(request):
     movie_objects = Moviedata.objects.all()
+    
+    paginator = Paginator(movie_objects,5)
+    page = request.GET.get('page')
+    movie_objects = paginator.get_page(page)
+    
     return render(request, 'newapp/movie_list.html', {'movie_objects': movie_objects})
 
